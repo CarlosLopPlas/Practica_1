efa3176c9ed0611bd69ecd129f1462c1b8134b17
CarlosLopPlas <124624803+CarlosLopPlas@users.noreply.github.com>
2023-09-23 00:20:37 -0600

    Add files via upload

---
+++ Practica_1_Carlos-LopPlas/login_registro/views/contenido.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='styleheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    <title>contenido</title>
+
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">PRACTICA 1</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <hr class= "border">
+        <div class="contenido">
+            <article>
+                <p>¿Que es ansible <br /> Ansible es una plataforma de software libre para configurar y administrar ordenadores. Combina instalación multi-nodo, ejecuciones de tareas ad hoc y administración de configuraciones. Adicionalmente, Ansible es categorizado como una herramienta de orquestación.​ </p>
+            
+                <p><br />¿Que es IA?<br />VLa inteligencia artificial, en el contexto de las ciencias de la computación, es una disciplina y un conjunto de capacidades cognoscitivas e intelectuales expresadas por sistemas informáticos. </p>
+            </article>
+        </div>    
+    </div>
+</body>
+</html>
